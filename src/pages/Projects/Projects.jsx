f3aa81c12cf4a3e835e30955ea32e99babd671c8
Abi Carson-Brown <abigailbrown.25@gmail.com>
2024-03-28 13:06:58 +0000

    Feat: Add CoffeeBean section

--- src/pages/Projects/Projects.jsx
+++ src/pages/Projects/Projects.jsx
@@ -1,3 +1,24 @@
+import { Link } from "react-router-dom";
+import "./Projects.scss";
+
 export default function Projects() {
-  return <h1>Projects</h1>;
+  return (
+    <main className="projects">
+      <h1 className="projects__title">Projects</h1>
+      <div className="project">
+        <Link
+          to="https://coffeebeen.abicarsonbrown.com"
+          className="project__link"
+          target="_blank"
+        >
+          CoffeeBeen
+        </Link>
+        <p className="project__description">
+          Bringing together front-end and back-end development skills to create
+          a web application to find and review coffee shops around London,
+          illustrated using the GoogleMaps API.
+        </p>
+      </div>
+    </main>
+  );
 }
