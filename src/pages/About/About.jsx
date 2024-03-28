b208b89179df886a105a7b688e55932048a94b0b
Abi Carson-Brown <abigailbrown.25@gmail.com>
2024-03-28 13:19:56 +0000

    Feat: Populate About page

--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -1,3 +1,45 @@
 export default function About() {
-  return <h1>About</h1>;
+  return (
+    <main className="about">
+      <h1 className="about__title">About</h1>
+      <p>
+        Hi, I'm Abi, a Software Engineer with a unique journey. My background
+        includes studying Mathematics at university and a rewarding 5-year
+        tenure in Fashion Merchandising, primarily at ASOS. Following my time at
+        ASOS, I ventured into other merchandising roles, but I yearned for a
+        more challenging path. With a passion for problem-solving and a knack
+        for diving into tasks headfirst, a friend introduced me to the world of
+        Software Engineering. This sparked my interest, leading me to enrol in a
+        BrainStation bootcamp. Now, with these new skills, both Front-End and
+        Back-End, under my belt, I’m excited to apply what I have learned to
+        real world projects. My mission is to bring joy to people's lives
+        through innovative software solutions.
+      </p>
+      <p>Languages/skills: </p>
+      <ul>
+        <li>HTML5</li>
+        <li>CSS</li>
+        <li>Sass</li>
+        <li>JavaScript</li>
+        <li>React.js</li>
+        <li>Document Object Model (DOM)</li>
+        <li>OOP</li>
+        <li>APIs</li>
+        <li>Node.js</li>
+        <li>Express.js</li>
+        <li>Web APIs</li>
+        <li>User Authentication</li>
+        <li>OAuth</li>
+        <li>MySQL</li>
+        <li>Knex</li>
+        <li>Heroku</li>
+        <li>GitHub</li>
+        <li>Agile</li>
+        <li>Jest</li>
+        <li>MS Excel</li>
+        <li>SquareSpace</li>
+        <li>Jira</li>
+      </ul>
+    </main>
+  );
 }
