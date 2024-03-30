ed4643381d65fbe6d2f69d77fa34cca6efee2b92
Abi Carson-Brown <abigailbrown.25@gmail.com>
2024-03-30 12:09:03 +0000

    Style: Resize about section

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,41 +1,26 @@
 import "./About.scss";
-import Intro from "../Intro/Intro";
+import Stack from "../Stack/Stack";
 
 export default function About() {
   return (
-    <section className="about">
+    <section className="about" id="about">
       <div className="about__container">
         <h1 className="about__title">Abi Carson-Brown</h1>
         <h2 className="about__subtitle">Software Engineer</h2>
-        <Intro />
-        <section className="stack">
-          <h3 className="stack__title">Languages/skills: </h3>
-          {/* <ul className="stack__list">
-          <li>HTML5</li>
-          <li>CSS</li>
-          <li>Sass</li>
-          <li>JavaScript</li>
-          <li>React.js</li>
-          <li>Document Object Model (DOM)</li>
-          <li>OOP</li>
-          <li>APIs</li>
-          <li>Node.js</li>
-          <li>Express.js</li>
-          <li>Web APIs</li>
-          <li>User Authentication</li>
-          <li>OAuth</li>
-          <li>MySQL</li>
-          <li>Knex</li>
-          <li>Heroku</li>
-          <li>GitHub</li>
-          <li>Agile</li>
-          <li>Jest</li>
-          <li>MS Excel</li>
-          <li>SquareSpace</li>
-          <li>Jira</li>
-          <li>Vite</li>
-        </ul> */}
-        </section>
+        <p className="about__content">
+          Hi, I'm Abi, a Software Engineer with a unique journey. My background
+          includes studying Mathematics at university and a rewarding 5-year
+          tenure in Fashion Merchandising, primarily at ASOS. Following my time
+          at ASOS, I ventured into other merchandising roles, but I yearned for
+          a more challenging path. With a passion for problem-solving and a
+          knack for diving into tasks headfirst, a friend introduced me to the
+          world of Software Engineering. This sparked my interest, leading me to
+          enrol in a BrainStation bootcamp. Now, with these new skills, both
+          Front-End and Back-End, under my belt, I’m excited to apply what I
+          have learned to real world projects. My mission is to bring joy to
+          people's lives through innovative software solutions.
+        </p>
+        <Stack />
       </div>
     </section>
   );
---
+++ src/components/Stack/Stack.jsx
@@ -0,0 +1,32 @@
+import "./Stack.scss";
+
+export default function Stack() {
+  <section className="stack">
+    <h3 className="stack__title">Languages/skills: </h3>
+    {/* <ul className="stack__list">
+    <li>HTML5</li>
+    <li>CSS</li>
+    <li>Sass</li>
+    <li>JavaScript</li>
+    <li>React.js</li>
+    <li>Document Object Model (DOM)</li>
+    <li>OOP</li>
+    <li>APIs</li>
+    <li>Node.js</li>
+    <li>Express.js</li>
+    <li>Web APIs</li>
+    <li>User Authentication</li>
+    <li>OAuth</li>
+    <li>MySQL</li>
+    <li>Knex</li>
+    <li>Heroku</li>
+    <li>GitHub</li>
+    <li>Agile</li>
+    <li>Jest</li>
+    <li>MS Excel</li>
+    <li>SquareSpace</li>
+    <li>Jira</li>
+    <li>Vite</li>
+  </ul> */}
+  </section>;
+}
