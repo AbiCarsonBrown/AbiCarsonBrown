43564b7911f55fde4dce970f35a7825eaa2fd886
Abi Carson-Brown <abigailbrown.25@gmail.com>
2024-03-30 11:36:32 +0000

    Style: Position & size intro section

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -6,22 +6,26 @@ export default function About() {
       <div className="about__container">
         <h1 className="about__title">Abi Carson-Brown</h1>
         <h2 className="about__subtitle">Software Engineer</h2>
-        <h3 className="about__heading">About</h3>
-        <p className="about__content">
-          Hi, I'm Abi, a Software Engineer with a unique journey. My background
-          includes studying Mathematics at university and a rewarding 5-year
-          tenure in Fashion Merchandising, primarily at ASOS. Following my time
-          at ASOS, I ventured into other merchandising roles, but I yearned for
-          a more challenging path. With a passion for problem-solving and a
-          knack for diving into tasks headfirst, a friend introduced me to the
-          world of Software Engineering. This sparked my interest, leading me to
-          enrol in a BrainStation bootcamp. Now, with these new skills, both
-          Front-End and Back-End, under my belt, I’m excited to apply what I
-          have learned to real world projects. My mission is to bring joy to
-          people's lives through innovative software solutions.
-        </p>
-        <p>Languages/skills: </p>
-        {/* <ul>
+        <section className="intro">
+          <h3 className="intro__title">About</h3>
+          <p className="intro__content">
+            Hi, I'm Abi, a Software Engineer with a unique journey. My
+            background includes studying Mathematics at university and a
+            rewarding 5-year tenure in Fashion Merchandising, primarily at ASOS.
+            Following my time at ASOS, I ventured into other merchandising
+            roles, but I yearned for a more challenging path. With a passion for
+            problem-solving and a knack for diving into tasks headfirst, a
+            friend introduced me to the world of Software Engineering. This
+            sparked my interest, leading me to enrol in a BrainStation bootcamp.
+            Now, with these new skills, both Front-End and Back-End, under my
+            belt, I’m excited to apply what I have learned to real world
+            projects. My mission is to bring joy to people's lives through
+            innovative software solutions.
+          </p>
+        </section>
+        <section className="stack">
+          <h3 className="stack__title">Languages/skills: </h3>
+          {/* <ul className="stack__list">
           <li>HTML5</li>
           <li>CSS</li>
           <li>Sass</li>
@@ -46,6 +50,7 @@ export default function About() {
           <li>Jira</li>
           <li>Vite</li>
         </ul> */}
+        </section>
       </div>
     </section>
   );
