b723ed56108fa6f2d7940f3bd7a785cf1bc0f4f1
Abi Carson-Brown <abigailbrown.25@gmail.com>
2024-03-30 11:39:54 +0000

    Refactor: Create Intro component

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,4 +1,5 @@
 import "./About.scss";
+import Intro from "../Intro/Intro";
 
 export default function About() {
   return (
@@ -6,23 +7,7 @@ export default function About() {
       <div className="about__container">
         <h1 className="about__title">Abi Carson-Brown</h1>
         <h2 className="about__subtitle">Software Engineer</h2>
-        <section className="intro">
-          <h3 className="intro__title">About</h3>
-          <p className="intro__content">
-            Hi, I'm Abi, a Software Engineer with a unique journey. My
-            background includes studying Mathematics at university and a
-            rewarding 5-year tenure in Fashion Merchandising, primarily at ASOS.
-            Following my time at ASOS, I ventured into other merchandising
-            roles, but I yearned for a more challenging path. With a passion for
-            problem-solving and a knack for diving into tasks headfirst, a
-            friend introduced me to the world of Software Engineering. This
-            sparked my interest, leading me to enrol in a BrainStation bootcamp.
-            Now, with these new skills, both Front-End and Back-End, under my
-            belt, I’m excited to apply what I have learned to real world
-            projects. My mission is to bring joy to people's lives through
-            innovative software solutions.
-          </p>
-        </section>
+        <Intro />
         <section className="stack">
           <h3 className="stack__title">Languages/skills: </h3>
           {/* <ul className="stack__list">
---
+++ src/components/Intro/Intro.jsx
@@ -0,0 +1,22 @@
+import "./Intro.scss";
+
+export default function Intro() {
+  return (
+    <section className="intro">
+      <h3 className="intro__title">About</h3>
+      <p className="intro__content">
+        Hi, I'm Abi, a Software Engineer with a unique journey. My background
+        includes studying Mathematics at university and a rewarding 5-year
+        tenure in Fashion Merchandising, primarily at ASOS. Following my time at
+        ASOS, I ventured into other merchandising roles, but I yearned for a
+        more challenging path. With a passion for problem-solving and a knack
+        for diving into tasks headfirst, a friend introduced me to the world of
+        Software Engineering. This sparked my interest, leading me to enrol in a
+        BrainStation bootcamp. Now, with these new skills, both Front-End and
+        Back-End, under my belt, I’m excited to apply what I have learned to
+        real world projects. My mission is to bring joy to people's lives
+        through innovative software solutions.
+      </p>
+    </section>
+  );
+}
