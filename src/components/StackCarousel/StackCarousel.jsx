51032fac38c5d5107eebd2c99455a01e31c58af5
Abi Carson-Brown <abigailbrown.25@gmail.com>
2024-03-31 22:46:00 +0100

    Style: Add stack icons

--- src/components/StackCarousel/StackCarousel.jsx
+++ src/components/StackCarousel/StackCarousel.jsx
@@ -1,15 +1,50 @@
 import { useEffect, useState } from "react";
 import "./StackCarousel.scss";
+import CSSIcon from "../../assets/icons/CSS3.svg";
+import Express from "../../assets/icons/Express.svg";
+import Git from "../../assets/icons/Git.svg";
+import GitHub from "../../assets/icons/GitHub.svg";
+import Heroku from "../../assets/icons/Heroku.svg";
+import HTMLIcon from "../../assets/icons/HTML5.svg";
+import JSIcon from "../../assets/icons/JavaScript.svg";
+import Jest from "../../assets/icons/Jest.svg";
+import Jira from "../../assets/icons/Jira.svg";
+import Knex from "../../assets/icons/Knex.svg";
+import MySQL from "../../assets/icons/MySQL.svg";
+import Node from "../../assets/icons/Node.svg";
+import ReactIcon from "../../assets/icons/React.svg";
+import Sass from "../../assets/icons/Sass.svg";
+import TypeScript from "../../assets/icons/TypeScript.svg";
+import Vercel from "../../assets/icons/Vercel.svg";
+import Vite from "../../assets/icons/Vite.svg";
 
 export default function StackCarousel() {
-  const stack = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17];
+  const stack = [
+    HTMLIcon,
+    CSSIcon,
+    JSIcon,
+    Sass,
+    ReactIcon,
+    TypeScript,
+    Git,
+    GitHub,
+    Node,
+    Express,
+    MySQL,
+    Knex,
+    Jira,
+    Vite,
+    Heroku,
+    Vercel,
+    Jest,
+  ];
 
   return (
     <div className="carousel">
       {stack.map((item, index) => {
         return (
           <div key={index} className="carousel__item">
-            item {item}
+            <img src={item} alt="" />
           </div>
         );
       })}
