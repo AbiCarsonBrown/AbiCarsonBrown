e85a00237240457783abd0c9434ad648648b4ba8
Abi Carson-Brown <abigailbrown.25@gmail.com>
2024-03-31 22:56:44 +0100

    Style: Reduce stack section size

--- src/components/Stack/Stack.jsx
+++ src/components/Stack/Stack.jsx
@@ -7,21 +7,6 @@ export default function Stack() {
       <h3 className="stack__title">My Tech Stack </h3>
       <StackCarousel />
       {/* <ul className="stack__list">
-    <li>HTML5</li>
-    <li>CSS</li>
-    <li>Sass</li>
-    <li>JavaScript</li>
-    <li>React.js</li>
-    <li>Node.js</li>
-    <li>Express.js</li>
-    <li>MySQL</li>
-    <li>Knex</li>
-    <li>Vite</li>
-    <li>Heroku</li>
-    <li>GitHub</li>
-    <li>Jira</li>
-    <li>Jest</li>
-
     <li>Document Object Model (DOM)</li>
     <li>OOP</li>
     <li>APIs</li>
