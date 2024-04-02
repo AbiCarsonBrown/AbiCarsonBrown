539f3c61667169d22ff685282bc7ac2bba1f527e
Abi Carson-Brown <abigailbrown.25@gmail.com>
2024-04-02 23:02:51 +0100

    Style: Add icons to contact footer

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,13 +1,25 @@
 import "./Contact.scss";
 import { Link } from "react-router-dom";
+import Email from "../../assets/icons/Email.svg";
+import LinkedIn from "../../assets/icons/LinkedIn.svg";
+import GitHub from "../../assets/icons/GitHub.svg";
 
 export default function Contact() {
   return (
     <footer className="contact">
       <h4 className="contact__title">Contact me:</h4>
-      <a href="mailto:abigailbrown.25@gmail.com">Email</a>
-      <Link to="www.linkedin.com/in/abi-carson-brown">LinkedIn</Link>
-      <Link to="https://github.com/AbiCarsonBrown">GitHub</Link>
+      <Link to="mailto:abigailbrown.25@gmail.com" className="contact__link">
+        <img src={Email} alt="Email icon" className="contact__icon" />
+        Email
+      </Link>
+      <Link to="www.linkedin.com/in/abi-carson-brown" className="contact__link">
+        <img src={LinkedIn} alt="LinkedIn icon" className="contact__icon" />
+        LinkedIn
+      </Link>
+      <Link to="https://github.com/AbiCarsonBrown" className="contact__link">
+        <img src={GitHub} alt="GitHub icon" className="contact__icon" />
+        GitHub
+      </Link>
     </footer>
   );
 }
