3620e03a8bc5bf5d7d2610a907c40c653d6fbc15
Abi Carson-Brown <abigailbrown.25@gmail.com>
2024-04-03 13:47:06 +0100

    Style: Initial contact footer layout for mobile

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,24 +1,33 @@
 import "./Contact.scss";
 import { Link } from "react-router-dom";
 import Email from "../../assets/icons/Email.svg";
-import LinkedIn from "../../assets/icons/LinkedIn.svg";
+import LinkedIn from "../../assets/icons/LinkedIn-black.svg";
 import GitHub from "../../assets/icons/GitHub.svg";
 
 export default function Contact() {
   return (
     <footer className="contact">
       <h4 className="contact__title">Contact me:</h4>
-      <Link to="mailto:abigailbrown.25@gmail.com" className="contact__link">
+      <Link
+        to="mailto:abigailbrown.25@gmail.com"
+        className="contact__link contact__link--email"
+      >
         <img src={Email} alt="Email icon" className="contact__icon" />
-        Email
+        <p className="contact__label">Email</p>
       </Link>
-      <Link to="www.linkedin.com/in/abi-carson-brown" className="contact__link">
+      <Link
+        to="www.linkedin.com/in/abi-carson-brown"
+        className="contact__link contact__link--in"
+      >
         <img src={LinkedIn} alt="LinkedIn icon" className="contact__icon" />
-        LinkedIn
+        <p className="contact__label">LinkedIn</p>
       </Link>
-      <Link to="https://github.com/AbiCarsonBrown" className="contact__link">
+      <Link
+        to="https://github.com/AbiCarsonBrown"
+        className="contact__link contact__link--git"
+      >
         <img src={GitHub} alt="GitHub icon" className="contact__icon" />
-        GitHub
+        <p className="contact__label">GitHub</p>
       </Link>
     </footer>
   );
