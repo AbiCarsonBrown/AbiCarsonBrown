3dc575d7ed3dc9eb8dfe8e01eebb06a12aa59926
Abi Carson-Brown <abigailbrown.25@gmail.com>
2024-04-08 13:48:39 +0100

    Style: Lay out About page for laptop

--- src/components/Stack/Stack.jsx
+++ src/components/Stack/Stack.jsx
@@ -6,15 +6,6 @@ export default function Stack() {
     <section className="stack">
       <h3 className="stack__title">My Tech Stack </h3>
       <StackCarousel />
-      {/* <ul className="stack__list">
-    <li>Document Object Model (DOM)</li>
-    <li>OOP</li>
-    <li>APIs</li>
-    <li>Web APIs</li>
-    <li>User Authentication</li>
-    <li>OAuth</li>
-    <li>Agile</li>
-  </ul> */}
     </section>
   );
 }
