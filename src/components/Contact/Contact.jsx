eb86a9ec700e5dbc1ce47ac91d23fdf79dbb694c
Abi Carson-Brown <abigailbrown.25@gmail.com>
2024-04-08 10:32:13 +0100

    Style: Size footer for larger mobiles

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -7,31 +7,33 @@ import GitHub from "../../assets/icons/GitHub-footer.svg";
 export default function Contact() {
   return (
     <footer className="contact">
-      <Link
-        to="mailto:abigailbrown.25@gmail.com"
-        className="contact__link contact__link--email"
-      >
-        <img src={Email} alt="Email icon" className="contact__icon" />
-        <p className="contact__label">Email</p>
-      </Link>
-      <Link
-        to="www.linkedin.com/in/abi-carson-brown"
-        className="contact__link contact__link--in"
-      >
-        <img
-          src={LinkedIn}
-          alt="LinkedIn icon"
-          className="contact__icon contact__icon--in"
-        />
-        <p className="contact__label">LinkedIn</p>
-      </Link>
-      <Link
-        to="https://github.com/AbiCarsonBrown"
-        className="contact__link contact__link--git"
-      >
-        <img src={GitHub} alt="GitHub icon" className="contact__icon" />
-        <p className="contact__label">GitHub</p>
-      </Link>
+      <div className="contact__container">
+        <Link
+          to="mailto:abigailbrown.25@gmail.com"
+          className="contact__link contact__link--email"
+        >
+          <img src={Email} alt="Email icon" className="contact__icon" />
+          <p className="contact__label">Email</p>
+        </Link>
+        <Link
+          to="www.linkedin.com/in/abi-carson-brown"
+          className="contact__link contact__link--in"
+        >
+          <img
+            src={LinkedIn}
+            alt="LinkedIn icon"
+            className="contact__icon contact__icon--in"
+          />
+          <p className="contact__label">LinkedIn</p>
+        </Link>
+        <Link
+          to="https://github.com/AbiCarsonBrown"
+          className="contact__link contact__link--git"
+        >
+          <img src={GitHub} alt="GitHub icon" className="contact__icon" />
+          <p className="contact__label">GitHub</p>
+        </Link>
+      </div>
     </footer>
   );
 }
